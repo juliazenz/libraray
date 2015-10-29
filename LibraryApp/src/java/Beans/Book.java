39781ee2d4fc2b6a30c3d181ea5af2f57d1e7141
pruta_000 <prutab12@htlkaindorf.at>
2015-10-29 15:11:16 +0100

    html und servlet

---
+++ LibraryApp/src/java/Beans/Book.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Beans;
+
+/**
+ *
+ * @author pruta_000
+ */
+public class Book {
+    private String picture;
+    private String title;
+    private String author; 
+    private boolean available;
+
+    public Book(String picture, String title, String author, boolean available) {
+        this.picture = picture;
+        this.title = title;
+        this.author = author;
+        this.available = available;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+    
+    
+}
--- LibraryApp/src/java/servlets/OverviewServlet.java
+++ LibraryApp/src/java/servlets/OverviewServlet.java
@@ -5,9 +5,18 @@
  */
 package servlets;
 
+import Beans.Book;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,6 +29,8 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "OverviewServlet", urlPatterns = {"/OverviewServlet"})
 public class OverviewServlet extends HttpServlet {
+
+    private LinkedList<Book> bookList = new LinkedList<>();
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -37,6 +48,31 @@ public class OverviewServlet extends HttpServlet {
             /* TODO output your page here. You may use following sample code. */
             RequestDispatcher rd = request.getRequestDispatcher("/html/overview.html");
             rd.include(request, response);
+
+            for (Book b : bookList) {
+                out.println("<tr>\n"
+                        + "                <td><img src='res/" + b.getPicture()+"'></td>"
+                        + "                <td>"+b.getTitle()+"</td>"
+                         + "                <td>"+b.getAuthor()+"</td>");
+                        if(b.isAvailable())
+                        {
+                            out.println("<td>Verf&uumlgbar</td>");
+                        }
+                        else
+                        {
+                            out.println("<td>nicht Verf&uumlgbar</td>");
+                        }
+                        out.println("</tr>");
+                        
+            }
+            out.println("    </table>");
+      
+
+            out.println("</body>\n"
+                    + "</html>");
+
+            
+            
         }
     }
 
@@ -53,6 +89,38 @@ public class OverviewServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config); //To change body of generated methods, choose Tools | Templates.
+        try {
+            loadData();
+        } catch (IOException ex) {
+            Logger.getLogger(OverviewServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void loadData() throws FileNotFoundException, IOException {
+        String path = this.getServletContext().getRealPath("/res" + File.separator + "buchdaten.csv");
+        FileReader fr = new FileReader(path);
+        BufferedReader br = new BufferedReader(fr);
+        String line = "";
+        String feld[] = null;
+        boolean available;
+        while ((line = br.readLine()) != null) {
+            feld = line.split(";");
+            //String picture, String title, String author, boolean available
+             available = false;
+            if (feld[3].equals("true")) {
+                available = true;
+            }
+            Book b = new Book(feld[0], feld[1], feld[2], available);
+            bookList.add(b);
+        }
+       
+        this.getServletContext().setAttribute("bookList", bookList);
+        br.close();
     }
 
     /**
